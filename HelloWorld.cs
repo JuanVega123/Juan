c1cdfa30f00a65aa265bac821f571bffedbe6634
JuanVega123 <elenderdmc@gmail.com>
2024-11-01 19:34:05 -0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //El print() sirve para imprimir el texto en la consola
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
